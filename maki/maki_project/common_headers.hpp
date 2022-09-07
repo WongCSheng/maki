6b1d5e7d5c0a2e2ff7bce6c3fa7c5b37528530ee
thea-sea <theasea.yw@gmail.com>
2022-09-07 12:32:04 +0800

    Created common_headers.hpp

--- maki/maki_project/common_headers.hpp
+++ maki/maki_project/common_headers.hpp
@@ -9,3 +9,4 @@
 #include "../lib/imgui/imgui.h"
 #include "../lib/imgui/imgui_impl_glfw.h"
 #include "../lib/imgui/imgui_impl_opengl3.h"
+#include "../lib/glfw-3.3.7.bin.WIN32/include/GLFW/glfw3.h"
--- maki/maki_project/main.cpp
+++ maki/maki_project/main.cpp
@@ -1,7 +1,31 @@
 #include "common_headers.hpp"
 
+GLFWwindow * window;
 int main()
 {
-	IMGUI_CHECKVERSION();
+	//IMGUI_CHECKVERSION();
+	//ImGui::CreateContext();
+	//ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//ImGui::StyleColorsDark();
+	//ImGui_ImplGlfw_InitForOpenGL(window, true);
+	//ImGui_ImplOpenGL3_Init("#version 330");
+
+	////Main while loop
+	//while (!glfwWindowShouldClose(window))
+	//{
+	//	//bg colour
+	//	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	//	//clean back buffer, assign new colour
+	//	glClear(GL_COLOR_BUFFER_BIT);
+
+	//	//tell imGui we are working on a new frame now
+	//	ImGui_ImplOpenGL3_NewFrame();
+	//	ImGui_ImplGlfw_NewFrame();
+	//	ImGui::NewFrame();
+
+	//
+
+	//}
+
 	return 0;
 }

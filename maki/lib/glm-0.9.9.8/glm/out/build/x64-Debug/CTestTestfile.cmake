147add6c58b2381db7ad9b988e1c63bfa3d21cc5
louie5277 <33411320+louie5277@users.noreply.github.com>
2022-09-13 18:18:51 +0800

    update graphics rendering to current main
    pull and update graphics branch from main

---
+++ maki/lib/glm-0.9.9.8/glm/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Admon/source/repos/maki/maki/lib/glm-0.9.9.8/glm
+# Build directory: C:/Users/Admon/source/repos/maki/maki/lib/glm-0.9.9.8/glm/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
+subdirs("test")

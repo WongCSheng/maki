abde65fb3e6f0dbddc492c2958162a468b221476
louie5277 <33411320+louie5277@users.noreply.github.com>
2022-09-25 17:29:00 +0800

    Fixed the shader and vert compilation errors

---
+++ maki/lib/rapidJSON/out/build/windows-default/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: D:/project/maki/lib/rapidJSON
+# Build directory: D:/project/maki/lib/rapidJSON/out/build/windows-default
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("doc")
+subdirs("example")
+subdirs("test")

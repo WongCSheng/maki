c8a7aa8e87a6808609a48e8d76c2d3d5bf1e28b6
refi-nnej <97603125+refi-nnej@users.noreply.github.com>
2022-11-05 22:41:01 +0800

    fix rttr in release for mergeattempt

--- Merge Attempt/maki/SushiMi/Engine/Serialiser/RTTR.cpp
+++ Merge Attempt/maki/SushiMi/Engine/Serialiser/RTTR.cpp
@@ -8,9 +8,9 @@ getters and setters for TestClass that is used to debug json parsing.
 *//*__________________________________________________________________________*/
 
 #pragma once
-#include "../include/common_headers.hpp"
+#include "../Headers/STL_Header.h"
+#include "../RTTR_Header.h"
 
-//rttr doesnt work yet
 //register your class for rttr here
 RTTR_REGISTRATION
 {
@@ -23,7 +23,34 @@ registration::class_<TestClass>("TestClass")
 .property("testNull", &TestClass::testNull)
 .property("testStr", &TestClass::testStr);
 
-}
+//	using namespace rttr;
+//registration::class_<Object>("Object")
+//.property("orientation", &Object::orientation)
+//.property("scaling", &Object::scaling)
+//.property("position", &Object::position)
+//.property("initialPos", &Object::initialPos)
+//.property("velocity", &Object::velocity)
+//.property("dirCurr", &Object::dirCurr)
+//.property("aabb", &Object::aabb)
+//.property("status", &Object::status)
+//.property("mass", &Object::mass)
+//.property("mdl_to_ndc_xform", &Object::mdl_to_ndc_xform)
+//.property("mdl_ref", &Object::mdl_ref)
+//.property("shd_ref", &Object::shd_ref)
+////.property("square", &Object::square) //we don't have any squares rn
+//.property("color", &Object::color)
+//.property("mdl_xform", &Object::mdl_xform)
+//.property("objects", &Object::objects)
+//.property("anim_rainbow", &Object::anim_rainbow)
+//.property("rainbowCount", &Object::rainbowCount)
+//.property("anim_bw", &Object::anim_bw)
+//.property("bwCount", &Object::bwCount)
+//.property("flip", &Object::flip)
+//.property("rot_right", &Object::rot_right)
+//.property("rot_left", &Object::rot_left)
+//.property("scale_up", &Object::scale_up)
+//.property("scale_down", &Object::scale_down);
+//}
 
 
 TestClass::TestClass()

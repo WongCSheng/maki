ad946d39f8c5ddf5866c4ca10d8426ad685923c8
SadPotato666 <aliciachong0417@gmail.com>
2022-11-28 22:30:49 +0800

    Revert "pull n push"
    This reverts commit 5a1d0553a825fcc9d23171270381f9c1e8566d81.

--- Merge Attempt/maki/lib/glm-0.9.9.8/glm/out/build/x64-Debug/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Aurel/Desktop/Maki_Clone/maki/Merge Attempt/maki/lib/glm-0.9.9.8/glm/glm
-# Build directory: C:/Users/Aurel/Desktop/Maki_Clone/maki/Merge Attempt/maki/lib/glm-0.9.9.8/glm/out/build/x64-Debug/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Merge Attempt/maki/lib/glm/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Aurel/Desktop/Maki_Clone/maki/Merge Attempt/maki/lib/glm
-# Build directory: C:/Users/Aurel/Desktop/Maki_Clone/maki/Merge Attempt/maki/lib/glm/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
-subdirs("test")

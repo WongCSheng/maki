0ade74a7c6db243ee20eac6b72323700f701122d
thea-sea <90663667+thea-sea@users.noreply.github.com>
2022-11-28 22:13:31 +0800

    deleted old maki, renamed file to Graphics (still have mem leaks from texture

--- maki/lib/rapidJSON/out/build/windows-default/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/project/maki/lib/rapidJSON
-# Build directory: D:/project/maki/lib/rapidJSON/out/build/windows-default
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("doc")
-subdirs("example")
-subdirs("test")

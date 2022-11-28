b211bb0865a7aa5532220fe1ef0b6d783881c4dd
SadPotato666 <aliciachong0417@gmail.com>
2022-11-28 22:31:32 +0800

    Revert "Revert "pull n push""
    This reverts commit ad946d39f8c5ddf5866c4ca10d8426ad685923c8.

---
+++ maki/lib/glm/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Aurel/Desktop/Maki_Clone/maki/maki/lib/glm
+# Build directory: C:/Users/Aurel/Desktop/Maki_Clone/maki/maki/lib/glm/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
+subdirs("test")

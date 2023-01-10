10662d957111ffc5328e326b986df8c77973cbe9
louishetong.wang <louishetong.wang@DIT0873SG>
2023-01-10 18:50:47 +0800

    merged now to try animating

--- Main/maki/SushiMi/Engine/Texture/Sprite.h
+++ Main/maki/SushiMi/Engine/Texture/Sprite.h
@@ -6,3 +6,30 @@
 
 @brief		Drawing of the sprite and animating of it is here
 *//*__________________________________________________________________________*/
+#include "../Engine/Texture/texture.h"
+#include "../Engine/System/Renderer.h"
+#include "../Engine/Components/Transform/sTransform.h"
+#include "../Engine/Animation/Animation2D.h"
+
+
+class Sprite
+{
+public:
+	Sprite(const char* filename);
+
+	~Sprite();
+
+	void draw();
+
+	void draw(double deltatime, AnimationType type);
+
+	void Add_animation(const char* filename);
+
+	Transform transformation;
+
+private:
+	Texture texture;
+	Core::vtx rectangle;
+
+	std::vector<Animation2D*> anims;
+};
--- Main/maki/SushiMi/Game Object/Player.cpp
+++ Main/maki/SushiMi/Game Object/Player.cpp
@@ -1,22 +1,24 @@
+/*!
+@file		player.cpp
+@author		louishetong.wang@digipen.edu
+@date		20/11/2022
+
+@brief		Creating a player using sprite and adding of animation. movement of player
+			, drawing and transformation of player is here too
+*//*__________________________________________________________________________*/
 #include "Player.h"
-#include "../Engine/Components/Transform/sTransform.h"
-#include "../Engine/Animation/Animation2D.h"
-#include "../Engine/Texture/Sprite.h"
-struct fakemat4
-{
-	glm::mat4 data;
-};
+
 Player::Player()
 {
+
 	sp = new Sprite("../textures/spritesheet/spritesheet.png");
-	sp->transformation = new Transform();
-	sp->transformation->position = glm::vec2(500, 500);
-	sp->transformation->scale = glm::vec2(150, 150);
+	sp->transformation.position = glm::vec2(500, 500);
+	sp->transformation.scale = glm::vec2(150, 150);
 
 	sp->Add_animation("../textures/spritesheet/Idle.txt");
 	sp->Add_animation("../textures/spritesheet/Run.txt");
 
-	current_anim = new AnimationType(Idle);
+	current_anim = Idle;
 }
 
 Player::~Player()
@@ -26,34 +28,51 @@ Player::~Player()
 
 void Player::move_left()
 {
-	if (sp->transformation->scale.x > 0)
-		sp->transformation->scale.x *= -1;
+	if (sp->transformation.scale.x > 0)
+		sp->transformation.scale.x *= 1;
 
-	current_anim = new AnimationType(Run);
-	sp->transformation->position.x -= 5;
+	current_anim = Run;
+	sp->transformation.position.x -= 1;
 }
 
 void Player::move_right()
 {
-	if (sp->transformation->scale.x < 0)
-		sp->transformation->scale.x *= -1;
+	if (sp->transformation.scale.x < 0)
+		sp->transformation.scale.x *= -1;
 
-	current_anim = new AnimationType(Run);
-	sp->transformation->position.x += 5;
+	current_anim = Run;
+	sp->transformation.position.x += 1;
+}
+
+void Player::move_up()
+{
+	if (sp->transformation.scale.y > 0)
+		sp->transformation.scale.y *= 1;
+
+	current_anim = Run;
+	sp->transformation.position.y -= 1;
+}
+
+void Player::move_down()
+{
+	if (sp->transformation.scale.y < 0)
+		sp->transformation.scale.y *= -1;
+
+	current_anim = Run;
+	sp->transformation.position.y += 1;
 }
 
 void Player::stop()
 {
-	current_anim = new AnimationType(Idle);
+	current_anim = Idle;
 }
 
-fakemat4* Player::Transformation()
+glm::mat4 Player::Transformation()
 {
-	fakemat4* temp = new fakemat4{ sp->transformation->Get() };
-	return temp;
+	return sp->transformation.Get();
 }
 
 void Player::draw(double deltatime)
 {
-	sp->draw(deltatime, *current_anim);
+	sp->draw(deltatime, current_anim);
 }

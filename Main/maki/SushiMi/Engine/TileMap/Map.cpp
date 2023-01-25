079267fb9113331c2124bbddf082b40200ed9f7f
louie5277 <33411320+louie5277@users.noreply.github.com>
2023-01-25 10:03:23 +0800

    added loading of text file for tilemap

--- Main/maki/SushiMi/Engine/TileMap/Map.cpp
+++ Main/maki/SushiMi/Engine/TileMap/Map.cpp
@@ -8,14 +8,8 @@
 #define GRID_COL 30
 #define GRID_ROW 30
 
-#define EMPTY 0
-#define WALL 1
-#define PLAYER 2
-#define MAKI 3
-#define GOAL 4
-
-int grid_row = 30;
-int grid_col = 30;
+int grid_row;
+int grid_col;
 
 int gGrids[GRID_ROW][GRID_COL];
 
@@ -32,14 +26,32 @@ namespace Core
 	int Core::Map::initMap(const char* Filename)
 	{
 		//Open File using ifstream
-		std::fstream fin(Filename);
+		std::ifstream fin(Filename);
 		if(!fin)
 		{
-			std::cout << "Unable to open csv file!";
+			std::cout << "Unable to open tilemap file!";
 			return 0;
 		}
-
-		return 0;
+		/*store the row and col of the tile*/
+		fin >> grid_row;
+		fin >> grid_col;
+		for(int r = 0; r < grid_row; r++)
+		{
+			for(int c = 0; c < grid_col; c++)
+			{
+				fin >> gGrids[r][c];
+			}
+		}
+		/*Testing whether is loaded correctly*/
+		for(int r = 0; r < grid_row; r++)
+		{
+			for(int c = 0; c < grid_col; c++)
+			{
+				std::cout << gGrids[r][c] << "\t";
+			}
+			std::cout << std::endl;
+		}
+		return 1;
 	}
 
 	void Core::Map::DrawMap()

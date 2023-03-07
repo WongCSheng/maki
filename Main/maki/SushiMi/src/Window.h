8c19d39a714e4be2e2fc2b744b2b2a95ca1995e1
WongCSheng <w.chongsheng@digipen.edu>
2023-03-07 17:44:36 +0800

    Revert "Revert "Merge branch 'M3-submission-branch' into M5-submission-branch""
    This reverts commit d5badcb40e8661d31b693cbbab6e691f51c106bf.

--- Main/maki/SushiMi/src/Window.h
+++ Main/maki/SushiMi/src/Window.h
@@ -64,6 +64,8 @@ namespace Core
 
 		static inline bool isDialogue;
 		static inline int dialogue_style;
+
+		static inline bool show_fps;
 		enum class dialogue
 		{
 			T1 = 0,
@@ -148,5 +150,6 @@ namespace Core
 		static void init();
 		static void cleanup();
 	};
-#define Get_Delta()				Window::GetInstance(0, 0)->getDelta()
+	#define Get_Delta()			Window::GetInstance(0,0)->getDelta()
 }
+
